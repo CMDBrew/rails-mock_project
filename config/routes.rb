07a2896a8261958ff8d27ce1ee9c024ec8799dc7
Nelson Lee <nelson@cleverbanana.com>
2017-11-04 15:40:47 -0700

    Setup layout components

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,9 @@ Rails.application.routes.draw do
   %w[404 401 500 503].each do |code|
     get code, to: 'errors#show', code: code
   end
+
+  get '/about'   => 'home#about',   as: :about
+  get '/pricing' => 'home#pricing', as: :pricing
+
+  root 'home#index'
 end
